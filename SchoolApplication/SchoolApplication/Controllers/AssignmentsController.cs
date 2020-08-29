3e40b694a28681f1bef73ed628daa54d153b31c5
Mehul Talajia <mehul.talajia@signify.com>
2020-08-29 10:57:22 +0530

    Assignments & Ratting controllers

---
+++ SchoolApplication/SchoolApplication/Controllers/AssignmentsController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Internal;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SchoolApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AssignmentsController : ControllerBase
+    {
+        [HttpGet]
+        public HttpResponseMessage Assignments(Guid CourseId)
+        {
+            //Returns the pre stored assignment provided by teacher for a particular course 
+
+            return  new HttpResponseMessage();
+        }
+
+        [HttpPost("/{courseId}/{studentId}")]
+        public Guid Assignments(Guid courseId , Guid studentId ,[FromBody] object assignmentFile)
+        {
+            //Uploaded Assignment can be store in Block storage with Id path of that in DB 
+            // which will be used for evaluating the assignment
+            
+            return Guid.NewGuid(); // New Assignment GUID 
+            //TODO: IT can be refined to send complete url and assignment ID in object so student can 
+            //latter check its assignment details
+        }
+    }
+}
---
+++ SchoolApplication/SchoolApplication/Controllers/RatingsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SchoolApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RatingsController : ControllerBase
+    {
+
+        [HttpGet]
+        public decimal Course(Guid CourseId)
+        {
+            return 0;
+        }
+
+        [HttpPost]
+        public HttpStatusCode Course(object CourseObject)
+        {
+            return HttpStatusCode.Accepted;
+        }
+    }
+}
